89a5571f42421b9354e2aa9d74ef10b54f7830e9
Jared Burnham <zlolris97@gmail.com>
2019-07-25 23:08:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,10 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pidgeons = {}
+  i = 0
+  data.each do |k, v|
+    v.each do |ke, va|
+      binding.pry
+    end
+  end
 end
